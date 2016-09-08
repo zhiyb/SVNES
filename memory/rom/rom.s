f463e31d3c9351524c0fa9a25c2673556ad18782
Yubo Zhi <normanzyb@gmail.com>
2016-09-08 21:12:32 +0800

    ASM: APU defines

--- memory/rom/rom.s
+++ memory/rom/rom.s
@@ -1,62 +1,67 @@
 ; asmsyntax=asmM6502
 
-	.segment "VECT"	; Interrupt vectors
-	.word	reset	; NMI
-	.word	reset	; Reset
-	.word	irq	; IRQ
+	.include	"apu.inc"
 
 	.bss
-irqcnt:	.res	1
+irqcnt:	.byte	0
+
+	.segment "VECT"	; Interrupt vectors
+	.word	nmi	; NMI
+	.word	main	; Reset
+	.word	irq	; IRQ
 
 	.code
-reset:	lda	#$ff
+	.reloc
+
+.proc	main
+	lda	#$ff
 	tax
 	txs
-	sta	$3004	; gpio1_dir
+	sta	$3004	; GPIO1_DIR
 	lda	#$00
 	tay
-	sta	$3005	; gpio1_out
+	sta	$3005	; GPIO1_OUT
 
-	lda	#$1f	; enable all channels
-	sta	$4015	; apu_status
-
-	lda	#$ce
-	sta	$4002	; apu_pulse1_tmrl
-
-	lda	#$08
-	sta	$4003	; apu_pulse1_lc
-
-	lda	#$ff	; duty 3, halt, constant
-	sta	$4000	; apu_pulse1_ctrl
-
-	lda	#$9f	; sweep, period 1, sub, shift 7
-	sta	$4001	; apu_pulse1_sweep
-
-	ldx	#$f0	; delay 4s
-	jsr	delay
-
-	lda	#$7f	; no halt
-	sta	$4008	; apu_triangle_linear
+	lda	#$1f	; Enable all channels
+	sta	apu_status
 
 	lda	#$ce
-	sta	$400a	; apu_triangle_tmrl
+	sta	apu_pulse1_tmrl
 
 	lda	#$08
-	sta	$400b	; apu_triangle_lc
+	sta	apu_pulse1_lc
 
-	ldx	#$3c	; delay 1s
+	lda	#$ff	; Duty 3, halt, constant
+	sta	apu_pulse1_ctrl
+
+	lda	#$9f	; Sweep, period 1, sub, shift 7
+	sta	apu_pulse1_sweep
+
+	ldx	#240	; Delay 4s
 	jsr	delay
 
-	lda	#$2f	; loop, envelope, period 15
-	sta	$400c	; apu_noise_ctrl
+	lda	#$7f	; No halt
+	sta	apu_tri_ctrl
+
+	lda	#$ce
+	sta	apu_tri_tmrl
+
+	lda	#$08
+	sta	apu_tri_lc
+
+	ldx	#60	; Delay 1s
+	jsr	delay
+
+	lda	#$2f	; Loop, envelope, period 15
+	sta	apu_noise_ctrl
 
 	lda	#$00
-	sta	$400f	; apu_noise_lc
+	sta	apu_noise_lc
 
 	lda	#$00
-noise0:	sta	$400e	; apu_noise_cfg
-	sta	$400f	; apu_noise_lc
-	ldx	#$1e	; delay 500ms
+noise0:	sta	apu_noise_period
+	sta	apu_noise_lc	; Reset envelope
+	ldx	#30	; Delay 500ms
 	jsr	delay
 	clc
 	adc	#$01
@@ -64,51 +69,55 @@ noise0:	sta	$400e	; apu_noise_cfg
 	bne	noise0
 
 	lda	#$80
-noise1:	sta	$400e	; apu_noise_cfg
-	sta	$400f	; apu_noise_lc
-	ldx	#$1e	; delay 500ms
+noise1:	sta	apu_noise_period
+	sta	apu_noise_lc	; Reset envelope
+	ldx	#30	; Delay 500ms
 	jsr	delay
 	clc
 	adc	#$01
 	cmp	#$90
 	bne	noise1
 
-	lda	#$1f	; no halt, constant, volume 15
-	sta	$400c	; apu_noise_ctrl
+	lda	#$1f	; No halt, constant, volume 15
+	sta	apu_noise_ctrl
 
 	lda	#$08
-	sta	$400f	; apu_noise_lc
+	sta	apu_noise_lc
 
-	lda	#$9f	; duty 2, no halt, constant
-	sta	$4004	; apu_pulse2_ctrl
+	lda	#$9f	; Duty 2, no halt, constant
+	sta	apu_pulse2_ctrl
 
 	lda	#$88
-	sta	$4006	; apu_pulse2_tmrl
+	sta	apu_pulse2_tmrl
 
-reload:	ldx	#$3c	; Delay 1s
+reload:	ldx	#60	; Delay 1s
 	jsr	delay
 	lda	#$10
-	sta	$4007	; APU_PULSE2_LC
+	sta	apu_pulse2_lc
 	iny
 	sty	$3005	; GPIO1_OUT
-	clv
-	bvc	reload
+	jmp	reload
+.endproc
 
-delay:	pha		; Push A
-	txa
-	pha		; Push X
+.proc	delay
+	pha		; Push A
+	stx	irqcnt
 	cli		; Waiting for 60Hz APU IRQ
-loop:	txa
+loop:	lda	irqcnt
 	bne	loop
 	sei
-	pla		; Pull X
-	tax
 	pla		; Pull A
 	rts		; Return
+.endproc
 
-irq:	pha		; Push A
-	dex
+.proc	nmi
+	rti
+.endproc
+
+.proc	irq
+	pha		; Push A
 	dec	irqcnt	; Decrement irqcnt
-	lda	$4015	; Clean frame interrupt
+	lda	apu_status	; Clean frame interrupt
 	pla		; Pull A
 	rti
+.endproc

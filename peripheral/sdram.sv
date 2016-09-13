370d89ac4007021d1d1d0170400de9ad1ece91a2
Yubo Zhi <normanzyb@gmail.com>
2016-09-13 23:16:27 +0800

    DRAM controller initial

--- peripheral/sdram.sv
+++ peripheral/sdram.sv
@@ -1,13 +1,45 @@
-module sdram #(parameter CS = 3) (
-	input logic clk, en,
+module sdram #(parameter CS = 3, INIT = 14300) (
+	input logic n_reset, clk, en,
 	output logic [12:0] DRAM_ADDR,
 	output logic [1:0] DRAM_BA, DRAM_DQM,
 	output logic DRAM_CKE, DRAM_CLK,
-	output logic DRAM_CS_N, DRAM_CAS_N, DRAM_RAS_N, DRAM_WE_N,
+	output logic DRAM_CS_N, DRAM_RAS_N, DRAM_CAS_N, DRAM_WE_N,
 	inout wire [15:0] DRAM_DQ
 );
 
 assign DRAM_CLK = ~clk;
 assign DRAM_CS_N = ~en;
+
+enum {NOP} command;
+always_comb
+begin
+	DRAM_CKE = 1'b0;
+	case (command)
+	NOP: begin
+		DRAM_CKE = 1'b1;
+		DRAM_RAS_N = 1'b1;
+		DRAM_CAS_N = 1'b1;
+		DRAM_WE_N = 1'b1;
+	end
+	endcase
+end
+
+enum {Reset, Init} state;
+logic [15:0] cnt;
+always_ff @(posedge clk, negedge n_reset)
+	if (~n_reset) begin
+		state <= Reset;
+		cnt <= 16'h0;
+	end else if (cnt == 16'h0)
+		case (state)
+		Reset: begin
+			state <= Init;
+			cnt <= INIT;
+		end
+		Init: state <= Init;
+		default: state <= Init;
+		endcase
+	else
+		cnt <= cnt - 16'h1;
 
 endmodule
--- wrapper.sv
+++ wrapper.sv
@@ -7,7 +7,7 @@ module wrapper (
 	output logic [12:0] DRAM_ADDR,
 	output logic [1:0] DRAM_BA, DRAM_DQM,
 	output logic DRAM_CKE, DRAM_CLK,
-	output logic DRAM_CS_N, DRAM_CAS_N, DRAM_RAS_N, DRAM_WE_N,
+	output logic DRAM_CS_N, DRAM_RAS_N, DRAM_CAS_N, DRAM_WE_N,
 	inout wire [15:0] DRAM_DQ,
 	
 	inout logic I2C_SCLK, I2C_SDAT,

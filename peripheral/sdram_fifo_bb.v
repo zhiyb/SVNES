4805a025ba1e3a910bd8fd746334453e99fcbd96
Yubo Zhi <normanzyb@gmail.com>
2017-10-23 19:47:18 +0100

    Update to Quartus 17.0

--- peripheral/sdram_fifo_bb.v
+++ peripheral/sdram_fifo_bb.v
@@ -1,4 +1,4 @@
-// megafunction wizard: %FIFO%
+// megafunction wizard: %FIFO%VBB%
 // GENERATION: STANDARD
 // VERSION: WM1.0
 // MODULE: dcfifo 
@@ -17,7 +17,6 @@
 // 17.0.2 Build 602 07/19/2017 SJ Lite Edition
 // ************************************************************
 
-
 //Copyright (C) 2017  Intel Corporation. All rights reserved.
 //Your use of Intel Corporation's design tools, logic functions 
 //and other software and tools, and its AMPP partner logic 
@@ -33,10 +32,6 @@
 //authorized distributors.  Please refer to the applicable 
 //agreement for further details.
 
-
-// synopsys translate_off
-`timescale 1 ps / 1 ps
-// synopsys translate_on
 module sdram_fifo (
 	aclr,
 	data,
@@ -66,46 +61,6 @@ module sdram_fifo (
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_on
 `endif
-
-	wire [31:0] sub_wire0;
-	wire  sub_wire1;
-	wire  sub_wire2;
-	wire [1:0] sub_wire3;
-	wire [31:0] q = sub_wire0[31:0];
-	wire  rdempty = sub_wire1;
-	wire  wrfull = sub_wire2;
-	wire [1:0] wrusedw = sub_wire3[1:0];
-
-	dcfifo	dcfifo_component (
-				.aclr (aclr),
-				.data (data),
-				.rdclk (rdclk),
-				.rdreq (rdreq),
-				.wrclk (wrclk),
-				.wrreq (wrreq),
-				.q (sub_wire0),
-				.rdempty (sub_wire1),
-				.wrfull (sub_wire2),
-				.wrusedw (sub_wire3),
-				.eccstatus (),
-				.rdfull (),
-				.rdusedw (),
-				.wrempty ());
-	defparam
-		dcfifo_component.intended_device_family = "Cyclone IV E",
-		dcfifo_component.lpm_numwords = 4,
-		dcfifo_component.lpm_showahead = "OFF",
-		dcfifo_component.lpm_type = "dcfifo",
-		dcfifo_component.lpm_width = 32,
-		dcfifo_component.lpm_widthu = 2,
-		dcfifo_component.overflow_checking = "OFF",
-		dcfifo_component.rdsync_delaypipe = 4,
-		dcfifo_component.read_aclr_synch = "OFF",
-		dcfifo_component.underflow_checking = "OFF",
-		dcfifo_component.use_eab = "ON",
-		dcfifo_component.write_aclr_synch = "OFF",
-		dcfifo_component.wrsync_delaypipe = 4;
-
 
 endmodule
 

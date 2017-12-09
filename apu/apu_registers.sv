b2ed12258624538ee91838f396b494f684fedbb7
Yubo Zhi <normanzyb@gmail.com>
2017-12-09 23:48:09 +0000

    apu/apu_registers: APU register read out

--- apu/apu_registers.sv
+++ apu/apu_registers.sv
@@ -10,13 +10,13 @@ module apu_registers (
 	output logic [7:0] regs[4]
 );
 
-assign we = sel & sys_rw;
-
 always_ff @(posedge dclk, negedge n_reset)
 	if (~n_reset)
 		for (int i = 0; i != 4; i++)
 			regs[i] <= 8'b0;
-	else if (we)
+	else if (sel & sys_rw)
 		regs[sys_addr[1:0]] <= sys_data;
+
+assign sys_data = sel & ~sys_rw ? regs[sys_addr[1:0]] : 8'bz;
 
 endmodule
